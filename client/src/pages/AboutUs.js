83c02838bd28aa0d269eb93259a9e5a22bf5c05b
raunak-bagaria <raunakbagaria.rb@gmail.com>
2024-11-18 20:20:57 +0530

    Add About Us page

---
+++ client/src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+export default function AboutUs() {
+    return (
+        <div className="aboutus">
+            <h1>About Us</h1> <br></br>
+            <h3>Welcome to ThoughtLane, your digital avenue for ideas, insights, and inspiration!</h3>
+            <div className="about-content">We are a vibrant blogging platform dedicated to sharing diverse perspectives on topics that matter—from technology and lifestyle to creativity and personal growth. <br></br> <br></br>
+
+                At ThoughtLane, we believe every thought deserves a lane to travel on, and we aim to connect readers and writers in a community fueled by curiosity and passion. Whether you're here to learn, reflect, or spark a conversation, we're here to guide your journey.
+                <br></br><br></br>
+                Join us in exploring the lanes of thought that shape our world.</div>
+        </div>
+    )
+}
